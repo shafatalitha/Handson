bbceb0e94ad8d5b72e7a9f8b69242c39edb4c3bc
shafatalitha <talitusapi@gmail.com>
2022-04-06 09:26:37 +0700

    use router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-
-import './App.css';
-import Search from './pages/Search';
-import {Provider} from 'react-redux';
-import Store from './Store'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Search from "./pages/Search";
+import Trending from "./pages/trending";
+import { Provider } from "react-redux";
+import Store from "./Store";
+import Navbar from "./pages/Navbar";
 function App() {
   return (
     <Provider store={Store}>
-      <Search />
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path="/trending" component={Trending} />
+          <Route path="/search" component={Search} />
+
+          <Route path="/">
+            <h1>Welcome to our homepage</h1>
+            <Link to="/search">Search</Link>
+            <Link to="/trending">Trending</Link>
+          </Route>
+        </Switch>
+      </Router>
+      {/* <Router>
+      <Switch>
+      <Route path='/trending' component={Trending} />
+      <Route exact path='/' component={Search} />
+          </Switch>
+          </Router> */}
+
+      {/* <Router>
+        <Switch>
+          <Route path="/Search"> */}
+      {/* <Search/> */}
+      {/* </Route>
+          <Route path="/Trending">
+            <Trending/>
+          </Route>
+
+        </Switch>
+      
+      </Router> */}
     </Provider>
   );
 }
 
 export default App;
-
